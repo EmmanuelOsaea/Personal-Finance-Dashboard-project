e668a6ef7915728be5236edb4d45311b4fd3a17b
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2024-08-11 00:09:08 +0100

    Update app.routing.module.ts

--- app.routing.module.ts
+++ app.routing.module.ts
@@ -1,1 +1,16 @@
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component'; // Import your Dashboard component
 
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  // Add more routes here as needed
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
