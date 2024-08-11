1296c9f3f54d7e48c5061ba9b9c464c35a473ba8
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2024-08-11 22:41:31 +0100

    Update goals.service.ts

--- goals.service.ts
+++ goals.service.ts
@@ -1,1 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { GoalsService, Goal } from './goals.service';
 
+@Component({
+  selector: 'app-goals',
+  templateUrl: './goals.component.html',
+  styleUrls: ['./goals.component.css']
+})
+export class GoalsComponent implements OnInit {
+  goals: Goal[] = [];
+
+  constructor(private goalsService: GoalsService) { }
+
+  ngOnInit(): void {
+    this.loadGoals();
+  }
+
+  loadGoals(): void {
+    this.goalsService.getGoals().subscribe(data => {
+      this.goals = data;
+    });
+  }
+
+  addGoal(title: string, description: string): void {
+    const newGoal: Goal = { title, description, completed: false };
+    this.goalsService.addGoal(newGoal);
+    this.loadGoals(); // Refresh the list
+  }
+
+  // Other methods to update or delete goals can be added here
+}
