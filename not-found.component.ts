0a73e6c44280203a199bc11c385e24e5ef3d218d
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2024-08-11 23:21:25 +0100

    Update not-found.component.ts

--- not-found.component.ts
+++ not-found.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent {
   // You can include any necessary logic here, or leave it empty if not needed
