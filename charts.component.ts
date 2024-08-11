af38af321e8a5294b2c74f41e3f0733861af5c17
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2024-08-11 01:01:30 +0100

    Update charts.component.ts

--- charts.component.ts
+++ charts.component.ts
@@ -1,1 +1,26 @@
 
+// charts.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ChartService } from '../services/chart.service';
+
+@Component({
+  selector: 'app-charts',
+  templateUrl: './charts.component.html',
+  styleUrls: ['./charts.component.scss']
+})
+export class ChartsComponent implements OnInit {
+  chartData: any;
+
+  constructor(private chartService: ChartService) { }
+
+  ngOnInit(): void {
+    this.chartService.getChartData().subscribe(data => {
+      this.chartData = data;
+      this.renderChart();
+    });
+  }
+
+  renderChart(): void {
+    // Logic to render your chart, e.g., using Chart.js or another library
+  }
+}
