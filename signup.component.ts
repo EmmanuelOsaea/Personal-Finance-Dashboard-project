bd7995e059020b715df9f20efd34bb0e2984357d
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2024-08-11 23:28:07 +0100

    Update signup.component.ts

--- signup.component.ts
+++ signup.component.ts
@@ -5,7 +5,7 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
   email: string = '';
