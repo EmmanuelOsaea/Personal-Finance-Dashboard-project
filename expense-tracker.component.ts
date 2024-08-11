edaa8ea852c3cc07176f06b20e3ab5cafcfe2c15
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2024-08-11 01:28:31 +0100

    Update expense-tracker.component.ts

--- expense-tracker.component.ts
+++ expense-tracker.component.ts
@@ -1,1 +1,30 @@
 
+// expense-tracker.component.ts
+import { Component } from '@angular/core';
+
+interface Expense {
+  description: string;
+  amount: number;
+  date: string;
+}
+
+@Component({
+  selector: 'app-expense-tracker',
+  templateUrl: './expense-tracker.component.html',
+  styleUrls: ['./expense-tracker.component.scss']
+})
+export class ExpenseTrackerComponent {
+  expenses: Expense[] = [];
+  newExpense: Expense = { description: '', amount: 0, date: '' };
+
+  addExpense(): void {
+    if (this.newExpense.description && this.newExpense.amount && this.newExpense.date) {
+      this.expenses.push({ ...this.newExpense });
+      this.newExpense = { description: '', amount: 0, date: '' };
+    }
+  }
+
+  deleteExpense(expense: Expense): void {
+    this.expenses = this.expenses.filter(e => e !== expense);
+  }
+}
